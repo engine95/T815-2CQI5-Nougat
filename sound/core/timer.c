b64fd51cb46787bfc227a5e12362fac883a5258b
engine95 <mustangsroc@gmail.com>
2017-11-16 05:30:06 -0700

    Revert "Compiling fix"
    This reverts commit ea3215732b3e4fa5696d850da00b8cc40ed12059.

--- sound/core/timer.c
+++ sound/core/timer.c
@@ -1863,6 +1863,18 @@ static long __snd_timer_user_ioctl(struct file *file, unsigned int cmd,
 }
 
 
+static long snd_timer_user_ioctl(struct file *file, unsigned int cmd,
+				 unsigned long arg)
+{
+	struct snd_timer_user *tu = file->private_data;
+	long ret;
+
+	mutex_lock(&tu->ioctl_lock);
+	ret = __snd_timer_user_ioctl(file, cmd, arg);
+	mutex_unlock(&tu->ioctl_lock);
+	return ret;
+}
+
 static int snd_timer_user_fasync(int fd, struct file * file, int on)
 {
 	struct snd_timer_user *tu;
